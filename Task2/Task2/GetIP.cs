3a92a2ad0c631d67c6fd3e4eaa7e2cb1a1511a48
Karina Sharova <khlkarina5@mail.ru>
2018-12-08 13:16:02 +0300

    test

---
+++ Task2/Task2/GetIP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class IP
+    {
+        public List<string> FindSequencesSimilarToIp(string sequenceOfCharacters)
+        {
+            List<string> sequencesSimilarToIp = new List<string>();
+            List<int> pointIndices = new List<int>();
+            int ind = -1;
+            int numberOfCharacters = sequenceOfCharacters.Length;
+            int startIndex = 0;
+            while (startIndex <= numberOfCharacters)
+            {
+                ind = sequenceOfCharacters.IndexOf(".", startIndex);
+                if (ind >= 0)
+                {
+                    pointIndices.Add(ind);
+                    ind++;
+                    startIndex = ind + 1;
+                }
+                else break;
+            }
+
+            int countOfPoints = 0;
+            foreach (var item in pointIndices)
+            {
+                countOfPoints++;
+            }
+
+            for (int i = 0; i < countOfPoints - 3; i++)
+            {
+                if ((pointIndices[i + 3] - pointIndices[i + 2] > 1 && pointIndices[i + 3] - pointIndices[i + 2] < 5) && 
+                    (pointIndices[i + 2] - pointIndices[i + 1] > 1 && pointIndices[i + 2] - pointIndices[i + 1] < 5) &&
+                    (pointIndices[i + 1] - pointIndices[i] > 1 && pointIndices[i + 1] - pointIndices[i] < 5))
+                {
+                    string estimatedIp = sequenceOfCharacters.Substring(pointIndices[i - 3], pointIndices[i+3]- pointIndices[i] + 3);
+                    sequencesSimilarToIp.Add(estimatedIp);
+                }
+
+            }
+
+            return sequencesSimilarToIp;
+        }
+
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            IP ip = new IP();
+            Console.WriteLine("Enter a sequence of characters: ");
+
+            //character string introduction
+            string sequenceOfCharacters = Console.ReadLine();
+            List<string> pointIndices = new List<string>();
+            pointIndices = ip.FindSequencesSimilarToIp(sequenceOfCharacters);
+
+            //output all ip
+            foreach (var item in pointIndices)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
